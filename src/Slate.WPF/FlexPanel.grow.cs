5b41d6f3dbd84e57fd0753bfbc6e2a9710deef50
lukewire129 <lukewire129@gmail.com>
2025-06-24 14:49:48 +0900

    FlexPanel 개선

---
+++ src/Slate.WPF/FlexPanel.grow.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Slate.WPF
+{
+    public partial class FlexPanel : Panel
+    {
+        // flex-grow AttachedProperty
+        public static readonly DependencyProperty GrowProperty =
+            DependencyProperty.RegisterAttached (
+                "Grow",
+                typeof (double),
+                typeof (FlexPanel),
+                new FrameworkPropertyMetadata (0.0, FrameworkPropertyMetadataOptions.AffectsParentMeasure | FrameworkPropertyMetadataOptions.AffectsParentArrange));
+
+        public static void SetGrow(UIElement element, double value) => element.SetValue (GrowProperty, value);
+        public static double GetGrow(UIElement element) => (double)element.GetValue (GrowProperty);
+    }
+}
