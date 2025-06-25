410e8dfde2f7df578d9ac4d79889dcc2dc3bf2ea
Pluvia <pluvia@pluviumterra.net>
2025-06-25 11:39:23 +0900

    BorderExtensions.cs 마무리

--- src/MarkupChain.WinUI3/BorderExtensions.cs
+++ src/MarkupChain.WinUI3/BorderExtensions.cs
@@ -1,38 +1,48 @@
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Media.Animation;
 
-namespace MarkupChain.WinUI3
+namespace MarkupChain.WinUI3;
+
+public static class BorderExtensions
 {
-    public static class BorderExtensions
+    public static Border Background(this Border bdr, Brush color)
     {
-        public static Border Background(this Border bdr, Brush color)
-        {
-            bdr.SetValue (Border.BackgroundProperty, color);
-            return bdr;
-        }
+        bdr.SetValue(Border.BackgroundProperty, color);
+        return bdr;
+    }
 
-        public static Border Padding(this Border bdr, Thickness padding)
-        {
-            bdr.SetValue (Border.PaddingProperty, padding);
-            return bdr;
-        }
-        public static Border CornerRadius(this Border bdr, CornerRadius cornerRadius)
-        {
-            bdr.SetValue (Border.CornerRadiusProperty, cornerRadius);
-            return bdr;
-        }
+    public static Border BackgroundSizing(this Border bdr, BackgroundSizing backgroundSizing)
+    {
+        bdr.SetValue(Border.BackgroundSizingProperty, backgroundSizing);
+        return bdr;
+    }
 
-        public static Border BorderBrush(this Border bdr, Brush color)
-        {
-            bdr.SetValue (Border.BorderBrushProperty, color);
-            return bdr;
-        }
+    public static Border Padding(this Border bdr, Thickness padding)
+    {
+        bdr.SetValue(Border.PaddingProperty, padding);
+        return bdr;
+    }
 
-        public static Border BorderThickness(this Border bdr, Thickness padding)
-        {
-            bdr.SetValue (Border.BorderThicknessProperty, padding);
-            return bdr;
-        }
+    public static Border CornerRadius(this Border bdr, CornerRadius cornerRadius)
+    {
+        bdr.SetValue(Border.CornerRadiusProperty, cornerRadius);
+        return bdr;
+    }
+
+    public static Border BorderBrush(this Border bdr, Brush color)
+    {
+        bdr.SetValue(Border.BorderBrushProperty, color);
+        return bdr;
+    }
+
+    public static Border BorderThickness(this Border bdr, Thickness padding)
+    {
+        bdr.SetValue(Border.BorderThicknessProperty, padding);
+        return bdr;
+    }
+    
+    public static Border ChildTransitionsProperty(this Border bdr, TransitionCollection transitions)
+    {
+        bdr.SetValue(Border.ChildTransitionsProperty, transitions);
+        return bdr;
     }
 }
--- src/MarkupChain.WinUI3/GlobalUsing.cs
+++ src/MarkupChain.WinUI3/GlobalUsing.cs
@@ -3,5 +3,6 @@ global using Microsoft.UI.Xaml.Controls;
 global using Microsoft.UI.Xaml.Controls.Primitives;
 global using Microsoft.UI.Xaml.Input;
 global using Microsoft.UI.Xaml.Media;
+global using Microsoft.UI.Xaml.Media.Animation;
 global using System;
 global using System.Collections.Generic;
