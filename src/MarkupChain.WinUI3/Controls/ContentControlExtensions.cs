4667db48f9c05a15769bc082bdaa596982890f67
lukewire129 <lukewire129@gmail.com>
2025-06-30 15:15:36 +0900

    수정 !

--- src/MarkupChain.WinUI3/Controls/ContentControlExtensions.cs
+++ src/MarkupChain.WinUI3/Controls/ContentControlExtensions.cs
@@ -2,7 +2,7 @@ namespace MarkupChain.WinUI3.Controls;
 
 public static class ContentControlExtensions
 {
-    public static T Content<T>(this T userControl, UIElement content) where T : UserControl
+    public static T Content<T>(this T userControl, UIElement content) where T : ContentControl
     {
         userControl.SetValue(ContentControl.ContentProperty, content);
         return userControl;
