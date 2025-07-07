aeede09e6ac91249bd57bfb9d163c3f26e65fbe6
Pluvia <pluvia@pluviumterra.net>
2025-07-07 18:31:08 +0900

    Add BrushExtensions

---
+++ src/MarkupChain.WinUI3/BrushExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace MarkupChain.WinUI3
+{
+    //an extension class which able to chaining -Property(=DependencyProperty) affix Properties.
+    public static class BrushExtensions
+    {
+        public static T Opacity<T>(this T brush, double opacity) where T : Brush
+        {
+            brush.SetValue(Brush.OpacityProperty, opacity);
+            return brush;
+        }
+        
+        public static T RelativeTransform<T>(this T brush, Transform relativeTransform) where T : Brush
+        {
+            brush.SetValue(Brush.RelativeTransformProperty, relativeTransform);
+            return brush;
+        }
+        
+        public static T Transform<T>(this T brush, Transform transform) where T : Brush
+        {
+            brush.SetValue(Brush.TransformProperty, transform);
+            return brush;
+        }
+    }
+}
